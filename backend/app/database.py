9a59d5df039286371ba0f1f3928e13b338e7761d
Kshu27 <webflask27@gmail.com>
2026-01-24 19:20:24 +0530

    changed from local to cloud databse

--- backend/app/database.py
+++ backend/app/database.py
@@ -2,8 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:kshu086@localhost/tech_society"# here in teh end mention db not server name
-
+# DATABASE_URL = "postgresql://postgres:kshu086@localhost/tech_society"# here in teh end mention db not server name
+DATABASE_URL="postgresql://postgres:kshumayak086@db.vwmssqfdfovwxzydxvwv.supabase.co:5432/postgres"
 engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(
